8691b2f4d1d62c7990261b664efa7e0fb8c8398b
Lin He <413600118@qq.com>
2020-08-20 15:04:03 -0700

    Fix model data format

---
+++ js/model.js
@@ -0,0 +1,29 @@
+const data = {
+  info200: {
+    professors: [
+      {
+        name: "Joe Schmo",
+        desc:
+          "My class will involve a lot of group work activities, participation is heavily emphasized. There will be no final tests.",
+        rate: 0,
+      },
+      {
+        name: "John Brown",
+        desc:
+          "This is a project based course with heavy lectures, attendance is required and prepare to read materials.",
+        rate: 0,
+      },
+      {
+        name: "Jane Smith",
+        desc:
+          "Tests will be the largest part of your grades, lecture videos are uploaded to Canvas, attendance is optional",
+        rate: 0,
+      },
+    ],
+    comments: [],
+    gpa: 3.6,
+    timeConsumed: 10,
+    readingLoad: 3000,
+    practiceProblem: null,
+  },
+};
