7a5f87dd31b599f3f20718e0e1cf6c1cf96b33e7
Lin He <413600118@qq.com>
2020-08-21 11:59:17 -0700

    Fix error handling

--- js/index.js
+++ js/index.js
@@ -9,32 +9,46 @@ document.getElementById("send-comments-btn").addEventListener("click", () => {
   const comment = textarea.value;
   if (!!comment && comment.length > 0) {
     textarea.value = "";
-    sendComment(comment);
     textarea.setCustomValidity("");
+    sendComment(comment);
   } else {
     textarea.setCustomValidity("Comment can not be empty!");
-    let feedback = document.querySelector("#commentFeedback");
-    feedback.textContent = "Comment can not be empty!";
+    textarea.classList.add("is-invalid");
   }
 });
 
-document.getElementById("search-course-btn").addEventListener("click", () => {
-  const dept = document.getElementById("input-dept").value;
-  const num = document.getElementById("input-num").value;
-  const courseList = getCourseList();
-  if (dept.length > 0 && num.length > 0) {
-    const courseName = `${dept}${num}`;
-    console.log(courseName, courseList);
+document.getElementById("comment-textarea").addEventListener("input", () => {
+  const textarea = document.getElementById("comment-textarea");
+  textarea.setCustomValidity("");
+  textarea.classList.remove("is-invalid");
+});
 
+document.getElementById("search-course-btn").addEventListener("click", () => {
+  const form = document.getElementById("course-search");
+  const inputField = document.getElementById("input-course");
+  const courseName = inputField.value;
+  const courseList = getCourseList();
+  console.log(courseName, courseList);
+  if (courseName.length > 0) {
     if (courseList.includes(courseName)) {
-      courseName.setCustomValidity("");
+      inputField.setCustomValidity("");
       changeCourse(courseName);
       renderCourse();
     } else {
       console.error(`${courseName} not in course lists`);
-      courseName.setCustomValidity("this course does not exist");
-      let feedback = document.querySelector("#searchFeedback");
-      feedback.textContent = "this course does not exist";
+      form.classList.add("was-validate");
+      inputField.setCustomValidity("This course does not exist");
+      inputField.classList.add("is-invalid");
     }
   }
+});
+
+document.getElementById("input-course").addEventListener("input", () => {
+  const form = document.getElementById("course-search");
+  const inputField = document.getElementById("input-course");
+
+  form.classList.remove("was-validate");
+  inputField.setCustomValidity("");
+  inputField.classList.remove("is-valid");
+  inputField.classList.remove("is-invalid");
 });
