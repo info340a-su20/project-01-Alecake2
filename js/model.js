db96aab48d76a8d8f7c79b6ab4d662f7c7433f35
Lin He <413600118@qq.com>
2020-08-20 17:25:33 -0700

    Add img to model

--- js/model.js
+++ js/model.js
@@ -2,21 +2,24 @@ const data = {
   info200: {
     professors: [
       {
-        name: "Joe Schmo",
-        desc:
-          "My class will involve a lot of group work activities, participation is heavily emphasized. There will be no final tests.",
-        rate: 0,
-      },
-      {
         name: "John Brown",
         desc:
           "This is a project based course with heavy lectures, attendance is required and prepare to read materials.",
+        img: "img/instructorone.jpg",
+        rate: 0,
+      },
+      {
+        name: "Joe Schmo",
+        desc:
+          "My class will involve a lot of group work activities, participation is heavily emphasized. There will be no final tests.",
+        img: "img/instructortwo.jpg",
         rate: 0,
       },
       {
         name: "Jane Smith",
         desc:
           "Tests will be the largest part of your grades, lecture videos are uploaded to Canvas, attendance is optional",
+        img: "img/instructorthree.jpg",
         rate: 0,
       },
     ],
